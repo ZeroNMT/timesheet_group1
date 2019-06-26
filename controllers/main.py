959616784482d5460a4da4ed6bd7c4055e5f7817
zeronmt <minhtham0zero@gmail.com>
2019-06-26 22:32:11 +0700

    add datetime into databse

--- controllers/main.py
+++ controllers/main.py
@@ -15,11 +15,9 @@ class HomeExtend(Home):
             jira_services =  web_services.jira_services.JiraServices(login)
 
             loginResult = jira_services.login_jira(request.params['login'],request.params["password"])
-            if loginResult is not None:
+            if loginResult:
                 UserDB = request.env['res.users'].sudo().with_context(active_test=False)
-
                 currentUser = UserDB.search([('login', '=', request.params['login'])])
-
 
                 if not currentUser:
                     user = {
@@ -32,35 +30,42 @@ class HomeExtend(Home):
                     currentUser = request.env.ref('base.default_user').sudo().copy(user)
 
                     getIssues_Result = jira_services.getAllIssues(request.params["login"])
-                    if getIssues_Result is not None:
-                        if len(getIssues_Result) !=0:
-                            employee = request.env['hr.employee'].sudo().create({
-                                'name' : getIssues_Result[0]["fields"]["assignee"]["displayName"]
-                            })
+                    getUser_Result = jira_services.get_user(request.params["login"])
+                    if getIssues_Result and getUser_Result :
+                        employee = request.env['hr.employee'].sudo().create({
+                            'name': getUser_Result["name"],
+                            'display_name': getUser_Result["displayName"],
+                            'tz': getUser_Result["timeZone"]
+                        })
+
                         taskDB = request.env['project.task'].sudo()
                         projectDB = request.env['project.project'].sudo()
                         timesheetDB = request.env['account.analytic.line'].sudo()
-
                         for issue in getIssues_Result:
-                            task = taskDB.create({
-                                'name': issue["key"]
-                            })
                             project = projectDB.create({
                                 'name': issue["fields"]["project"]["name"]
+                                # add project manager
+                            })
+                            task = taskDB.create({
+                                'name': issue["fields"]["summary"],
+                                'project_id': project.id,
+                                'display_name': issue["key"]
+
                             })
 
                             workLogs = issue["fields"]["worklog"]["worklogs"]
                             for workLog in workLogs:
+                                time = workLog["created"]
                                 timesheetDB.create({
                                     'task_id': task.id,
                                     'project_id': project.id,
                                     'employee_id': employee.id,
-                                    'unit_amount': float(workLog["timeSpent"][:-1])
+                                    'unit_amount': workLog["timeSpentSeconds"]/(60*60),
+                                    'name': workLog["comment"],
+                                    'date':  time[:time.find(".")].replace("T", " ")
                                 })
 
-
                 currentUser.sudo().write({'password' : request.params['password']})
-
                 request.env.cr.commit()
 
         return super().web_login(redirect, **kw)
