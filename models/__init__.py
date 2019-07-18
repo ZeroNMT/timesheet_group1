19924bcae92eeeeeac2a88646824d8f944dc17b1
zeronmt <minhtham0zero@gmail.com>
2019-07-18 20:42:14 +0700

    add report  menuitem

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,6 @@ from . import timesheet_line
 from . import users
 from . import project
 from . import task
-from . import report
+from . import report_task
+from . import report_all_employee
+from . import report_project_employee
