ce894bb8931a5b50d6f32c6d1eac4d90b3313e05
zeronmt <minhtham0zero@gmail.com>
2019-07-18 22:19:38 +0700

    add crypt authorization

--- services/aes_cipher.py
+++ services/aes_cipher.py
@@ -14,13 +14,13 @@ class AESCipher():
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+        return base64.b64encode(iv + cipher.encrypt(raw)).decode("utf-8")
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
     def decrypt(self, enc):
-        enc = base64.b64decode(enc)
+        enc = base64.b64decode(enc.encode("utf-8"))
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
