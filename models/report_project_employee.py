bc01236cde0dc8dfb2272b3f7603a4d930f7f6d5
khangcero <nguyenankhangc01ld@gmail.com>
2019-07-19 00:02:01 +0700

    edit report task_project and create report employee_project

--- models/report_project_employee.py
+++ models/report_project_employee.py
@@ -20,58 +20,86 @@ class TimesheetProjectEmployeeReport(models.AbstractModel):
             SELECT
                    "project_project".key, "project_project".name,
                     sum("account_analytic_line".unit_amount), "project_project".id
-            FROM account_analytic_line LEFT JOIN project_project 
+            FROM project_project LEFT JOIN account_analytic_line
             ON "account_analytic_line".project_id = "project_project".id
             GROUP BY "project_project".id
         """
         self.env.cr.execute(sql_query_project)
         results_project = self.env.cr.dictfetchall()
-
+        total_all_project = 0.00
         if line_id is None:
             for project in results_project:
                 if project["key"]:
+                    total_all_project += project["sum"]
                     lines.append({
                         'id': str(project["id"]),
                         'name': "[%s] %s" % (project["key"], project["name"]),
                         'level': 2,
                         'unfoldable': True,
                         'unfolded': False,
-                        'columns': [{'name': project["sum"]}]
+                        'columns': [{'name': '%.2f' % project["sum"]}]
                     })
 
-
-
         if line_id:
-            sql_query_task = """
+            sql_query_project_in_line = """
                 SELECT
-                     "project_task".key, "project_task".name, sum("account_analytic_line".unit_amount)
-                FROM project_task LEFT JOIN  account_analytic_line 
-                ON "project_task".project_id = %s
-                        AND "account_analytic_line".task_id = "project_task".id 
-                GROUP BY "project_task".id
-                ORDER BY "project_task".key ASC 
+                       "project_project".key, "project_project".name,
+                        sum("account_analytic_line".unit_amount), "project_project".id
+                FROM project_project LEFT JOIN account_analytic_line 
+                ON "project_project".id = %s AND "account_analytic_line".project_id = "project_project".id
+                WHERE "project_project".key IS NOT NULL
+                GROUP BY "project_project".id
             """
-            sql_query_task = sql_query_task % line_id
-            self.env.cr.execute(sql_query_task)
-            results_task = self.env.cr.dictfetchall()
+            sql_query_project_in_line = sql_query_project_in_line % line_id
+            self.env.cr.execute(sql_query_project_in_line)
+            results_project_in_line = self.env.cr.dictfetchall()
 
-            print(results_task)
+
+            sql_query_employee = """
+                SELECT
+                     "hr_employee".name, sum("account_analytic_line".unit_amount)
+                FROM hr_employee  LEFT JOIN account_analytic_line
+                ON "account_analytic_line".employee_id = "hr_employee".id
+                WHERE "account_analytic_line".id_jira IS NOT NULL
+                GROUP BY "account_analytic_line".employee_id, "hr_employee".name
+            """
+            sql_query_employee = sql_query_employee
+            self.env.cr.execute(sql_query_employee)
+            results_employee = self.env.cr.dictfetchall()
+            print(results_employee)
+
             lines.append({
                 'id': line_id,
-                'name': 'Timesheet',
+                'name': "[%s] %s" % (results_project_in_line[0]["key"], results_project_in_line[0]["name"]),
                 'level': 2,
                 'unfoldable': True,
                 'unfolded': True,
-                'columns': [{'name': ''}]
+                'columns': [{'name': '%.2f' %results_project_in_line[0]["sum"]}]
             })
-            for task in results_task:
-                if task["key"]:
-                    lines.append({
-                        'id': 'task_%s' % task["key"],
-                        'name': "[%s] %s" % (task["key"], task["name"]),
-                        'level': 4,
-                        'parent_id': line_id,
-                        'columns': [{'name': task["sum"]}]
-                    })
-
+            total_employees = 0.00
+            for employee in results_employee:
+                total_employees += 0.0 if employee['sum'] is None else employee['sum']
+                lines.append({
+                    'id': 'task_%s' % employee["name"],
+                    'name': employee["name"],
+                    'level': 4,
+                    'parent_id': line_id,
+                    'columns': [{'name': '%.2f' %0.00 if employee['sum'] is None else '%.2f' %employee['sum']}]
+                })
+            lines.append({
+                'id': 'total_%s' % line_id,
+                'class': 'o_account_reports_domain_total',
+                'name': 'Total',
+                'level': 3,
+                'parent_id': line_id,
+                'columns': [{'name': '%.2f' % total_employees}]
+            })
+        if total_all_project and not line_id:
+            lines.append({
+                'id': 'total_project',
+                'class': 'o_account_reports_domain_total',
+                'name': 'Total',
+                'level': 1,
+                'columns': [{'name': '%.2f' % total_all_project}]
+            })
         return lines
--- models/report_task.py
+++ models/report_task.py
@@ -26,22 +26,35 @@ class TimesheetTaskReport(models.AbstractModel):
         """
         self.env.cr.execute(sql_query_project)
         results_project = self.env.cr.dictfetchall()
-
+        total_all_project = 0.00
         if line_id is None:
             for project in results_project:
                 if project["key"]:
+                    total_all_project += project["sum"]
                     lines.append({
                         'id': str(project["id"]),
                         'name': "[%s] %s" % (project["key"], project["name"]),
                         'level': 2,
                         'unfoldable': True,
                         'unfolded': False,
-                        'columns': [{'name': project["sum"]}]
+                        'columns': [{'name': '%.2f' % project["sum"]}]
                     })
 
-
-
         if line_id:
+            sql_query_project_in_line = """
+                SELECT
+                       "project_project".key, "project_project".name,
+                        sum("account_analytic_line".unit_amount), "project_project".id
+                FROM project_project LEFT JOIN account_analytic_line 
+                ON "project_project".id = %s AND "account_analytic_line".project_id = "project_project".id
+                WHERE "project_project".key IS NOT NULL
+                GROUP BY "project_project".id
+            """
+            sql_query_project_in_line = sql_query_project_in_line % line_id
+            self.env.cr.execute(sql_query_project_in_line)
+            results_project_in_line = self.env.cr.dictfetchall()
+
+
             sql_query_task = """
                 SELECT
                      "project_task".key, "project_task".name, sum("account_analytic_line".unit_amount)
@@ -55,23 +68,39 @@ class TimesheetTaskReport(models.AbstractModel):
             self.env.cr.execute(sql_query_task)
             results_task = self.env.cr.dictfetchall()
 
-            print(results_task)
             lines.append({
                 'id': line_id,
-                'name': 'Timesheet',
+                'name': "[%s] %s" % (results_project_in_line[0]["key"], results_project_in_line[0]["name"]),
                 'level': 2,
                 'unfoldable': True,
                 'unfolded': True,
-                'columns': [{'name': ''}]
+                'columns': [{'name': '%.2f' %results_project_in_line[0]["sum"]}]
             })
+            total_tasks = 0.00
             for task in results_task:
                 if task["key"]:
+                    total_tasks += 0.0 if task['sum'] is None else task['sum']
                     lines.append({
                         'id': 'task_%s' % task["key"],
                         'name': "[%s] %s" % (task["key"], task["name"]),
                         'level': 4,
                         'parent_id': line_id,
-                        'columns': [{'name': task["sum"]}]
+                        'columns': [{'name': '%.2f' %0.00 if task['sum'] is None else '%.2f' %task['sum']}]
                     })
-
+            lines.append({
+                'id': 'total_%s' % line_id,
+                'class': 'o_account_reports_domain_total',
+                'name': 'Total',
+                'level': 3,
+                'parent_id': line_id,
+                'columns': [{'name': '%.2f' % total_tasks}]
+            })
+        if total_all_project and not line_id:
+            lines.append({
+                'id': 'total_project',
+                'class': 'o_account_reports_domain_total',
+                'name': 'Total',
+                'level': 1,
+                'columns': [{'name': '%.2f' % total_all_project}]
+            })
         return lines
